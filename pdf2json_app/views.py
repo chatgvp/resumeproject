239f30129e7dc2877fb0a92b1cea93d11d6757f4
George Vincent Peña <gpena8826@gmail.com>
2023-07-20 20:47:49 +0800

    commit from

--- pdf2json_app/views.py
+++ pdf2json_app/views.py
@@ -6,6 +6,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+from transformers import AutoTokenizer, LlamaForCausalLM
+
+model = LlamaForCausalLM.from_pretrained("gpena8826/resumeproject")
+tokenizer = AutoTokenizer.from_pretrained("gpena8826/resumeproject")
+
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(
+    inputs.input_ids, attention_mask=inputs.attention_mask, max_length=30
+)
+generated_text = tokenizer.decode(generate_ids[0], skip_special_tokens=True)
+
+print(generated_text)
 
 
 class PdfToJsonView(APIView):
@@ -20,23 +35,6 @@ class PdfToJsonView(APIView):
             return Response(response_data, content_type="application/json")
         else:
             return Response({"error": "No PDF file was uploaded."}, status=400)
-
-
-# class PdfToJsonView(APIView):
-#     def post(self, request, *args, **kwargs):
-#         if "pdf_file" in request.FILES:
-#             pdf_file = request.FILES["pdf_file"]
-#             file_path = default_storage.save(
-#                 pdf_file.name, ContentFile(pdf_file.read())
-#             )
-#             response_data = {
-#                 "file_name": pdf_file.name,
-#                 "file_path": file_path,
-#                 "file_size": pdf_file.size,
-#             }
-#             return Response(response_data)
-#         else:
-#             return Response({"error": "No PDF file was uploaded."}, status=400)
 
 
 def home(request):
